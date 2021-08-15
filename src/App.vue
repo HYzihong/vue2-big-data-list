35c67199e64971f3471172507e81c1b316f43228
hy <zihong2020@foxmail.com>
2021-08-15 21:24:48 +0800

    add elementui

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
- * @Date: 2021-08-11 22:57:37
- * @LastEditTime: 2021-08-12 21:31:40
+ * @Date: 2021-08-15 19:29:32
+ * @LastEditTime: 2021-08-15 20:13:50
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue2-big-data-list/src/App.vue
@@ -9,16 +9,23 @@
 <template>
   <div id="app">
     <HelloWorld msg="vue2 大数据 展示 性能优化" />
+    <component :is="comp" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+import BigDataSelect from "./components/BigDataSelect/index";
 export default {
   name: "App",
   components: {
     HelloWorld,
+    BigDataSelect,
+  },
+  data() {
+    return {
+      comp: "BigDataSelect",
+    };
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-15 19:29:32
+ * @LastEditTime: 2021-08-15 20:12:07
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vue2-big-data-list/src/main.js
+ */
 import Vue from "vue";
 import App from "./App.vue";
-
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
   render: (h) => h(App),
